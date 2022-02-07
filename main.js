5135e679ec42174167f50652a1948e75c1236c27
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-02-07 16:29:17 +0300

    Updated UI fixed form control

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ const addBookModal = document.querySelector(".addBook");
 const booksContainer = document.querySelector("#books");
 
 const newBookBtn = document.querySelector(".newBookBtn");
-newBookBtn.addEventListener("click", () => {
+newBookBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   addBookModal.style.display =
     addBookModal.style.display === "flex" ? "none" : "flex";
 });
@@ -26,10 +27,13 @@ addBookBtn.addEventListener("click", (e) => {
   bookAuthorInput.value = "";
   bookPagesInput.value = "";
   updateLibrary();
+  addBookModal.style.display =
+    addBookModal.style.display === "flex" ? "none" : "flex";
 });
 
 const closeModalBtn = document.querySelector(".closeModalBtn");
-closeModalBtn.addEventListener("click", () => {
+closeModalBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   addBookModal.style.display =
     addBookModal.style.display === "flex" ? "none" : "flex";
 });
@@ -87,73 +91,74 @@ function deleteBook(id) {
   updateLibrary();
 }
 
-addBook(createId(), "keko", "kekocan", 21, false);
-addBook(createId(), "maho", "mahoaga", 121, true);
-addBook(createId(), "Sekesen günde devri alem", "emir", 356, false);
-
 function clearLibrary() {
   booksContainer.replaceChildren();
 }
 
 function updateLibrary() {
   clearLibrary();
+  if (libArray.length > 0) {
+    for (let book of libArray) {
+      const bookDiv = document.createElement("div");
+      bookDiv.className = "book";
 
-  for (let book of libArray) {
-    const bookDiv = document.createElement("div");
-    bookDiv.className = "book";
+      const bookTitleDiv = document.createElement("div");
+      bookTitleDiv.className = "title";
+      bookTitleDiv.textContent = book.title;
 
-    const bookTitleDiv = document.createElement("div");
-    bookTitleDiv.className = "title";
-    bookTitleDiv.textContent = book.title;
+      const bookAuthorDiv = document.createElement("div");
+      bookAuthorDiv.className = "author";
+      bookAuthorDiv.textContent = book.author;
 
-    const bookAuthorDiv = document.createElement("div");
-    bookAuthorDiv.className = "author";
-    bookAuthorDiv.textContent = book.author;
+      const bookPagesDiv = document.createElement("div");
+      bookPagesDiv.className = "pages";
+      bookPagesDiv.textContent = book.pages;
 
-    const bookPagesDiv = document.createElement("div");
-    bookPagesDiv.className = "pages";
-    bookPagesDiv.textContent = book.pages;
+      // const bookReadDiv = document.createElement("div");
+      // bookReadDiv.className = "read";
+      // bookReadDiv.textContent = book.read;
 
-    // const bookReadDiv = document.createElement("div");
-    // bookReadDiv.className = "read";
-    // bookReadDiv.textContent = book.read;
+      const bookIdDiv = document.createElement("div");
+      bookIdDiv.className = "id";
+      bookIdDiv.textContent = `id : ${book.id}`;
 
-    const bookIdDiv = document.createElement("div");
-    bookIdDiv.className = "id";
-    bookIdDiv.textContent = `id : ${book.id}`;
+      const buttonsDiv = document.createElement("div");
+      buttonsDiv.className = "actions";
 
-    const buttonsDiv = document.createElement("div");
-    buttonsDiv.className = "actions";
+      const deleteBtn = document.createElement("button");
+      deleteBtn.className = "deleteBtn";
+      deleteBtn.textContent = "Delete";
+      deleteBtn.addEventListener("click", () => {
+        deleteBook(book.id);
+      });
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.className = "deleteBtn";
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener("click", () => {
-      deleteBook(book.id);
-    });
+      const updateBtn = document.createElement("button");
+      updateBtn.className = "updateBtn";
+      updateBtn.textContent = "Completed";
+      updateBtn.style.backgroundColor = book.read
+        ? "rgb(37, 182, 37)"
+        : "rgb(136, 136, 140)";
+      updateBtn.addEventListener("click", () => {
+        book.updateReadStatus();
+        updateLibrary();
+      });
 
-    const updateBtn = document.createElement("button");
-    updateBtn.className = "updateBtn";
-    updateBtn.textContent = "Completed";
-    updateBtn.style.backgroundColor = book.read
-      ? "rgb(37, 182, 37)"
-      : "rgb(136, 136, 140)";
-    updateBtn.addEventListener("click", () => {
-      book.updateReadStatus();
-      updateLibrary();
-    });
+      buttonsDiv.appendChild(deleteBtn);
+      buttonsDiv.appendChild(updateBtn);
 
-    buttonsDiv.appendChild(deleteBtn);
-    buttonsDiv.appendChild(updateBtn);
+      bookDiv.appendChild(bookTitleDiv);
+      bookDiv.appendChild(bookAuthorDiv);
+      bookDiv.appendChild(bookPagesDiv);
+      // bookDiv.appendChild(bookReadDiv);
+      bookDiv.appendChild(bookIdDiv);
+      bookDiv.appendChild(buttonsDiv);
 
-    bookDiv.appendChild(bookTitleDiv);
-    bookDiv.appendChild(bookAuthorDiv);
-    bookDiv.appendChild(bookPagesDiv);
-    // bookDiv.appendChild(bookReadDiv);
-    bookDiv.appendChild(bookIdDiv);
-    bookDiv.appendChild(buttonsDiv);
-
-    booksContainer.appendChild(bookDiv);
+      booksContainer.appendChild(bookDiv);
+    }
+  } else {
+    const noBooks = document.createElement("div");
+    noBooks.textContent = "THERE IS NO BOOKS. READ SOME!!!";
+    booksContainer.appendChild(noBooks);
   }
 }
 
